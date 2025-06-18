db8c5d40fdb30892904bc96dd12333c085060fe4
Iyan SM <iyansyeedmiller@gmail.com>
2025-06-18 01:38:04 -0700

    added portfolio page

--- namesake/src/routes/+page.svelte
+++ namesake/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Website Starter</h1>
+<p>This is the homepage</p>
+<a href='./portfolio'>portfolio</a>
