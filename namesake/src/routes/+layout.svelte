547a0129097476608dd1b7890183f743f7a5b8b0
Iyan SM <iyansyeedmiller@gmail.com>
2025-06-30 20:44:12 -0700

    restyled main page

--- namesake/src/routes/+layout.svelte
+++ namesake/src/routes/+layout.svelte
@@ -3,38 +3,63 @@
 </script>
 
 <nav>
-   <div id='links'>
+    <div class='bar short'>
       <a href="/">Home</a>
-      <p>✦</p>
+    </div>
+    <div class='bar med'>
       <a href="/portfolio">Portfolio</a>
-      <p>✦</p>
+    </div>
+    <div class='bar long'>
       <a href="/games">Games</a>
-   </div>
-   <hr>
+    </div>
 </nav>
 
-{@render children()}
+<div style="padding:20px">
+    {@render children()}
+</div>
 
 <style>
  nav{
     /*border-style: dashed;
     border-color: red;*/
+    position: absolute;
+    left: 73%;
     font-family: "Fugaz One", sans-serif;
     font-weight: 400;
     font-style: normal;
     font-size: x-large;
+    writing-mode: vertical-rl;
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: left;
+    justify-content: right;
+    row-gap: 2rem;
+    margin-right: 1.2rem;
+    margin-left: 1.2rem;
+    margin-bottom: 1.2rem;
  }
 
- #links{
-    display: flex;
-    align-items: center;
-    justify-content: right;
-    column-gap: 3rem;
-    margin-top: 10px;
-    margin-right: 25px;
-    margin-left: 25px;
-    /*box-shadow: inset 0 -4px;*/
+ .bar{
+    border-radius: 0px 0px 10px 10px;
+    text-align: right;
+    align-content: center;
+    width: 3rem;
  }
+
+ .short{
+    background-color: #E6BCCD;
+    height: 10rem;
+ }
+
+ .med{
+    background-color: #D295BF;
+    height: 17rem;
+ }
+ .long{
+    background-color: #7E52A0;
+    height: 24rem;
+ }
+
  hr{
    width: 35rem;
    margin-right: 0px;
@@ -44,6 +69,8 @@
  a{
     text-decoration: none;
     color: white;
+    margin-bottom: 1rem;
+
  }
  p{
     color: #C163A4;
--- namesake/src/routes/+page.svelte
+++ namesake/src/routes/+page.svelte
@@ -1,13 +1,22 @@
 <br>
 <br>
-<h1>Iyan</h1>
-<h1>Syeed-Miller</h1>
+<header>
+	<h1>Iyan</h1>
+	<h1>Syeed-Miller</h1>
+</header>
 <br>
-<div id=bars>
-	<div class="bar"><span class="long"></span><i class="fab fa-github"></i> <span>@Fireg53</span></div> 
-	<div class="bar med"></div>
-	<div class="bar short"></div>
+<div class="links">
+	<button><i class="fab fa-github"></i><span>@Fireg53</span></button>
+	<button><i class="fab fa-youtube"></i> <span>@fireg5355</span></button>
+	<button><i class="fab fa-square-instagram"></i> <span>@iyan.in3d</span></button>
+	<button><i class="fas fa-envelope"></i> <span>@iyansyeedmiller@gmail.com</span></button>
 </div>
+<div class="resume">
+</div>
+
+
+
+
 <style>
 	h1{
 		font-family: "Climate Crisis", sans-serif;
@@ -16,9 +25,20 @@
 		font-style: normal;
 		font-variation-settings:
 			"YEAR" 1979;
-		color: white;
-		font-size: 6rem;
+		color: #29274C;
+		font-size: 6.5vw;
 		line-height: 2rem;
-		margin-left: 40px;
+		margin-left: 5%;
+		margin-right: 10%;
+		margin-top: 4rem;
+	}
+	.links{
+		display: flex;
+		column-gap: 2rem;
+		color: #29274C;
+		margin-left: 5%;
+	}
+	button{
+		/**/
 	}
 </style>
