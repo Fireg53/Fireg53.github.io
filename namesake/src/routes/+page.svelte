a2b4156ef5b5372a6bd034d31c2949ec3f075eaf
Iyan SM <iyansyeedmiller@gmail.com>
2025-07-01 17:05:45 -0700

    added resume and configured for cloudfare deployment

--- namesake/src/routes/+page.svelte
+++ namesake/src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+	function openInNewTab(url) {
+		window.open(url, '_blank').focus();
+	}
+</script>
+
 <br>
 <br>
 <header>
@@ -6,16 +12,21 @@
 </header>
 <br>
 <div class="links">
-	<button><i class="fab fa-github"></i><span>@Fireg53</span></button>
-	<button><i class="fab fa-youtube"></i> <span>@fireg5355</span></button>
-	<button><i class="fab fa-square-instagram"></i> <span>@iyan.in3d</span></button>
-	<button><i class="fas fa-envelope"></i> <span>@iyansyeedmiller@gmail.com</span></button>
+	<button onclick={openInNewTab("https://github.com/Fireg53")}><i class="fab fa-github"></i><span>@Fireg53</span></button>
+	<button onclick={openInNewTab("https://www.youtube.com/@fireg5355")}><i class="fab fa-youtube"></i> <span>@fireg5355</span></button>
+	<button onclick={openInNewTab("https://www.instagram.com/iyan.in3d/")}><i class="fab fa-square-instagram"></i> <span>@iyan.in3d</span></button>
+	<button onclick={openInNewTab("mailto:iyansyeedmiller@gmail.com")}><i class="fas fa-envelope"></i> <span>@iyansyeedmiller@gmail.com</span></button>
 </div>
 <div class="resume">
+	<p>Resume</p> <p>Download</p><i class="fab fa-youtube"></i>
+	<iframe
+	src="Resume.pdf#toolbar=0"
+	width= 100%
+	height= 100%
+	style="border:none"
+	title="Non-downloadable PDF Viewer"
+	></iframe>
 </div>
-
-
-
 
 <style>
 	h1{
@@ -38,7 +49,17 @@
 		color: #29274C;
 		margin-left: 5%;
 	}
-	button{
-		/**/
+	.resume{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 20px;
+		margin-top: 5rem;
+		margin-right:40%;
+		background-image: linear-gradient( #E6BCCD, #D295BF, #7E52A0);
+		width: 55vw;
+		height: 70vw;
+		border-radius: 20px;
+		margin-left: 5%;
 	}
 </style>
